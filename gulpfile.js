e7c8838890978ae8507a51575af6dd9d15b33cab
Sohan Fernando <sohan.fernando@registercentrum.se>
2017-02-20 08:57:59 +0100

    meh

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,23 @@ const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync').create();
 const proxy = require('http-proxy-middleware');
 
-let apiServer = null;
+var apiServer = null;
 
 gulp.task('reload:dev', function() {
     browserSync.reload();
 });
 
-gulp.task('start:api', (cb) => {
+gulp.task('start:api', () => {
     apiServer = nodemon({
         script: 'dev-server.js'
     });
-    cb();
 });
 
 gulp.task('watch', function() {
     return gulp.watch('./src/**/*', ['reload:dev']);
 });
 
-gulp.task('serve', ['watch'], (cb) => {
+gulp.task('serve', ['watch'], cb => {
     browserSync.init({
         server: {
             port: 3000,
@@ -30,10 +29,9 @@ gulp.task('serve', ['watch'], (cb) => {
     cb();
 });
 
-process.on('exit', function () {
+process.on('exit', function() {
     // In case the gulp process is closed (e.g. by pressing [CTRL + C]) stop both processes
-    apiServer.kill();
+    apiServer && apiServer.kill();
 });
 
-
-gulp.task('default', ['serve','start:api']);
+gulp.task('default', ['serve', 'start:api']);
